92a8ed062525a9496259fadaafab63af81a21c13
SylvainStak <stackersap@gmail.com>
2024-03-27 18:25:47 +0000

    update_config

--- blocks.h
+++ blocks.h
@@ -1,11 +1,22 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*Icon*/	/*Command*/		      /*Interval*/	/*Signal*/
+	{"",      "weather.py",	             900,	         13},
+	{"󰘚 ",    "memory.sh",	              30,	         12},
+	{"",      "battery.sh",	              30,	         11},
+	{"",      "net_traffic.sh",	           1,	         10},
+	{"",      "internet.sh",               5,	          9},
+  {"󰌌 ",    "keyboard_layout.sh",        0,           8},
+	{"",      "volume.sh",	               0,	          7},
+	{"󰃭 ",    "calendar.sh",					     5,	          6},
+  {" ",    "time.sh",                   1,           5},
+  {"",     "emoji.sh",                  0,           4},
+  {"󰸉",     "wallpaper.sh",              0,           3},
+  {"󰍹",     "monitors.sh",               0,           2},
+  {"󰐥",     "boot_options.sh",           0,           1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+//static char delim[] = " | ";
+//static unsigned int delimLen = 5;
+static char *delim = " | ";
